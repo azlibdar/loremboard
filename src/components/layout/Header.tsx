75fd7a054f6fc4e505b8d660350919f4886c7a19
Azlan Ibrahim <azlibdar@gmail.com>
2024-11-29 22:30:42 +0530

    enhance Header component for dark mode support

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,13 +10,13 @@ interface Props {
 const Header = ({ onMobileSidebarOpen }: Props) => {
   const { toggleTheme } = useContext(ThemeContext);
   return (
-    <header className="bg-white border-b border-zinc-100">
+    <header className="bg-white dark:bg-zinc-900 border-b border-zinc-100 dark:border-zinc-800">
       <div className="max-w-7xl mx-auto flex justify-between items-center py-2.5 px-4">
         <div className="flex items-center gap-1.5">
           <IconButton onClick={onMobileSidebarOpen} size="compact" variant="tertiary" className="md:hidden">
             <Sidebar size={20} strokeWidth={1.75} />
           </IconButton>
-          <h2 className="text-sm font-medium text-zinc-800">Loremboard</h2>
+          <h2 className="text-sm font-medium text-zinc-800 dark:text-zinc-200">Loremboard</h2>
         </div>
         <IconButton onClick={toggleTheme} size="compact">
           <Sun size={18} strokeWidth={1.75} />
