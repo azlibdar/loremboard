d43981d3f5e5a7a0668554e4af179e2359b8727e
Azlan Ibrahim <azlibdar@gmail.com>
2024-11-29 20:21:33 +0530

    add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
